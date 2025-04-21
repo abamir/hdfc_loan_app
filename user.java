af8f206c25a2e88371793c2fb3289b8cad52fafd
Amir <abdulamiruser@gmail.com>
2025-04-21 23:35:26 +0530

    JIRA - 102 completed as add Display() method to user.java class

--- user.java
+++ user.java
@@ -1,8 +1,18 @@
 public class user {
 
     int i = 10;
-    boolean = false;
-    int j=20;
-    String name = "Amir"
+    boolean isActive = false;
+    int j = 20;
+    String name = "Amir";
     int id = 101;
+
+    // JIRA - 102
+    public void display() {
+        System.out.println("Name: " + name);
+        System.out.println("ID: " + id);
+        System.out.println("Is Active: " + isActive);
+        System.out.println("I: " + i);
+        System.out.println("J: " + j);
+    }
+
 }
